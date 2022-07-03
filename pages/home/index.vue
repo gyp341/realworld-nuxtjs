9b70f26ce420ece460efa5e19051fbcf6c7f53aa
gyp341 <gyp341@126.com>
2022-07-03 17:33:48 +0800

    first release and deploy

--- pages/home/index.vue
+++ pages/home/index.vue
@@ -4,7 +4,7 @@
     <div class="banner">
       <div class="container">
         <h1 class="logo-font">拉勾教育</h1>
-        <p>A place to share your knowledge.</p>
+        <p>A place to share your knowledge..</p>
       </div>
     </div>
 
